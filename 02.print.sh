333abd06c0d3affe4db74381e3ac694e2b359a7a
Imrangithubrit <devopsandaws2023@gmail.com>
2023-05-28 15:46:34 +0530

    change done

--- 02.print.sh
+++ 02.print.sh
@@ -3,6 +3,7 @@ echo hello world
 
 echo -e "\e[31m Hello in red color\e[0m"
 
-echo -e '\e[32m hello in green color\e[0m"
+echo -e "\e[32m hello in green color\e[0m"
 
-echo -e "\e[33m Hello in yellow color\e[0m"
+
+
