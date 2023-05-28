96562d3097208fbeff2e6b169c1159d04b40d3d4
Imrangithubrit <devopsandaws2023@gmail.com>
2023-05-28 20:13:36 +0530

    color change

--- 02.print.sh
+++ 02.print.sh
@@ -5,7 +5,9 @@ echo -e "\e[31m Hello in red color\e[0m"
 
 echo -e "\e[32m hello in green color\e[0m"
 
+echo -e "\e[33m hello in yello color\e[0m"
+
+echo -e "\e[34m hello in yello color\e[0m"
 echo -e "\e[35m hello in yello color\e[0m"
-
-
+echo -e "\e[36m hello in yello color\e[0m"
 
